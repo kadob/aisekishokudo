103aeb3c2a025ebed74c6af7a7cee9f67f05c36b
kadob <ec1022fr@ed.ritsumei.ac.jp>
2023-10-30 00:58:51 +0900

    文言変更

--- resources/views/maps/map.blade.php
+++ resources/views/maps/map.blade.php
@@ -5,7 +5,7 @@
     <main>
         <!--マップ機能ここから-->
         <div id="map"></div>
-        <script src="{{ asset('/mapspot/mapspot.js') }}"></script>
+        <script src="{{ asset('/mapspot/mapspot.js') }}"></script><!--読み込み-->
         <script src="https://maps.googleapis.com/maps/api/js?language=ja&region=JP&key={{config('services.map_key')}}&callback=initMap" async defer></script>
         <!--マップ機能ここまで-->
     </main>
